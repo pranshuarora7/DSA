8447d0382e68d98c47c8f1648c15c044e0a2c461
Pranshu <161811419821x@gmail.com>
2023-10-20 23:51:03 +0530

    Added two codeforces solutions

--- Codeforces/D2Q5.cpp
+++ Codeforces/D2Q5.cpp
@@ -1,62 +1,34 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 int main()
 {
-    char ch[150];
-    int i, l, c1 = 0, c2 = 0, c3 = 0;
-    gets(ch);
-    l = strlen(ch);
-    for (i = 0; i < l; i++)
+    string s;
+    cin >> s;
+
+    vector<int> numbers;
+    for (int i = 0; i < s.length(); i += 2)
     {
-        if (ch[i] == '1')
+        numbers.push_back(s[i] - '0');
+    }
+
+    sort(numbers.begin(), numbers.end());
+
+    string rearrangedSum;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        rearrangedSum += to_string(numbers[i]);
+        if (i < numbers.size() - 1)
         {
-            c1++;
-        }
-        else if (ch[i] == '2')
-        {
-            c2++;
-        }
-        else if (ch[i] == '3')
-        {
-            c3++;
+            rearrangedSum += '+';
         }
     }
-    for (i = 0; i < c1; i++)
-    {
-        cout << '1';
-        if ((c2 == 0 && c3 == 0) && i == c1 - 1)
-        {
-            cout << endl;
-        }
-        else
-        {
-            cout << '+';
-        }
-    }
-    for (i = 0; i < c2; i++)
-    {
-        cout << '2';
-        if (i == c2 - 1 && c3 == 0)
-        {
-            cout << endl;
-        }
-        else
-        {
-            cout << '+';
-        }
-    }
-    for (i = 0; i < c3; i++)
-    {
-        cout << '3';
-        if (i == c3 - 1)
-        {
-            cout << endl;
-        }
-        else
-        {
-            cout << '+';
-        }
-    }
+
+    cout << rearrangedSum << endl;
+
     return 0;
 }
